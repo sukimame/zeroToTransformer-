4002130d773266dfa7e9fb9a37f2e89f248f0e4e
sukimame4 <sukimame4@gmail.com>
2025-09-19 16:34:55 +0900

    順伝播完成

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-from model import model
+from model import Model
+
+f1 = open("jp_corpus.txt", "r", encoding="utf-8")
+f2 = open("en_corpus.txt", "r", encoding="utf-8")
+jp_corpus = f1.read()
+jp_corpus = jp_corpus.split("\n")
+en_corpus = f2.read()
+en_corpus = en_corpus.split("\n")
+
+model = Model()
+print(model.forward(jp_corpus[1], en_corpus[1]).shape)
+
+print(jp_corpus[1])
+
+
--- model.py
+++ model.py
@@ -7,17 +7,22 @@ def softmax(x):
     x = x - np.max(x, axis=-1, keepdims=True)
     return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
 
-class model:
-    def __init__(self, v_len, dmodel=512):
+class Model:
+    def __init__(self, dmodel=512):
         self.encoder = [EncoderMainLayer() for _ in range(6)]
         self.decoder = [DecoderMainLayer() for _ in range(6)]
-        
+
         self.dmodel = dmodel
         
+        self.Em = InputProc()
         rng = np.random.default_rng()
-        self.W = rng.random((512, v_len))
+        self.W = rng.random((512, self.Em.sp.GetPieceSize()))
     
     def forward(self, inputs, outputs):
+        
+        inputs = self.Em.BPE(inputs)
+        outputs = self.Em.BPE(outputs)
+        
         z_en = inputs + InputProc.positional_encoding(*inputs.shape)
         z_de = outputs + InputProc.positional_encoding(*outputs.shape)
         for layer in self.encoder:
@@ -32,7 +37,7 @@ class model:
 if __name__ == "__main__":
     inputs = np.ones((64, 512))
     outputs = np.ones((64, 512))
-    m = model(1000)
+    m = Model()
     print(m.forward(inputs, outputs).shape)
         
 
